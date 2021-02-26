519dfda27bab6b1aeb563de8bb58226c4d8bc49d
Arthur Hassan Souki <souki.arthur@gmail.com>
2021-02-25 23:12:30 -0300

    exercicio finalizado

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,10 +1,24 @@
 def sub_strings(text, dictionary)
+    if text.empty?
+        puts "Empty Text!"
+        return 
+    end
+
+    words = text.split
+    words_hash =  Hash.new
     
+    words.each do |word|
+        if dictionary.include?(word.downcase)
+            words_hash[word] ? words_hash[word] = words_hash[word] + 1 : words_hash[word] =  1
+        end
+    end
+
+    puts words_hash.empty? ? "None of the words or phrases are contained in the dictionary" : words_hash
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-print "Digite uma palavra ou frase para ser pesquisada no dicionario: "
+print "Write a word or a phrase: "
 text = gets.chomp
 
 sub_strings(text, dictionary)
