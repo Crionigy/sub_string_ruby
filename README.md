cbf40fddd3afd6d6c8b6307f38002d61c5eabf22
Arthur Hassan Souki <souki.arthur@gmail.com>
2021-02-25 22:32:04 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Projeto em ruby sobre sub strings em ruby
+
+## Sobre esse repositorio
+
+Esse repositorio é um projeto proposto pelo curso "The Odin Project" durante suas aulas sobre ruby.
+
+## Uso
+
+Basta ter o ruby instalado e usar o comando:
+
+    ruby <nome_do_arquivo>
---
+++ sub_strings.rb
@@ -0,0 +1,10 @@
+def sub_strings(text, dictionary)
+
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+print "Digite uma palavra ou frase para ser pesquisada no dicionario: "
+text = gets.chomp
+
+sub_strings(text, dictionary)
